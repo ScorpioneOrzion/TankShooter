93a038128598dd181c39b49733efc68f2168c98c
ScorpioneOrzion <36512390+ScorpioneOrzion@users.noreply.github.com>
2021-10-05 14:01:56 +0200

    basic setup done

--- index.js
+++ index.js
@@ -1,45 +1,76 @@
-function bodyToMesh(mesh, body) {
-  mesh.position.x = body.position.x;
-  mesh.position.y = body.position.y;
-  mesh.position.z = body.position.z;
-  mesh.quaternion.x = body.quaternion.x;
-  mesh.quaternion.y = body.quaternion.y;
-  mesh.quaternion.z = body.quaternion.z;
-  mesh.quaternion.w = body.quaternion.w;
+function setPosition(mesh, body) {
+  mesh.position.copy(body.position);
+  mesh.quaternion.copy(body.quaternion);
 }
 
-// Setup our world
-var world = new CANNON.World();
-world.gravity.set(0, 0, -9.82); // m/s²
+var world, mass, body, shape, timeStep = 1 / 60,
+  camera, scene, renderer, geometry, material, mesh;
 
-// Create a sphere
-var radius = 1; // m
-var sphereBody = new CANNON.Body({
-   mass: 5, // kg
-   position: new CANNON.Vec3(0, 0, 10), // m
-   shape: new CANNON.Sphere(radius)
-});
-world.addBody(sphereBody);
+initThree();
+initCannon();
+animate();
 
-// Create a plane
-var groundBody = new CANNON.Body({
-    mass: 0 // mass == 0 makes the body static
-});
-var groundShape = new CANNON.Plane();
-groundBody.addShape(groundShape);
-world.addBody(groundBody);
+function initCannon() {
 
-var fixedTimeStep = 1.0 / 60.0; // seconds
-var maxSubSteps = 3;
+  world = new CANNON.World();
+  world.gravity.set(0, 0, 0);
+  world.broadphase = new CANNON.NaiveBroadphase();
+  world.solver.iterations = 10;
 
-// Start the simulation loop
-var lastTime;
-(function simloop(time){
-  requestAnimationFrame(simloop);
-  if(lastTime !== undefined){
-     var dt = (time - lastTime) / 1000;
-     world.step(fixedTimeStep, dt, maxSubSteps);
-  }
-  console.log("Sphere z position: " + sphereBody.position.z);
-  lastTime = time;
-})();
+  shape = new CANNON.Box(new CANNON.Vec3(1, 1, 1));
+  mass = 1;
+  body = new CANNON.Body({
+    mass: 1
+  });
+  body.addShape(shape);
+  body.angularVelocity.set(0, 10, 0);
+  body.angularDamping = 0.5;
+  world.addBody(body);
+
+}
+
+function initThree() {
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100);
+  camera.position.z = 5;
+  scene.add(camera);
+
+  geometry = new THREE.BoxGeometry(2, 2, 2);
+  material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false });
+
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  document.body.appendChild(renderer.domElement);
+
+}
+
+function animate() {
+
+  requestAnimationFrame(animate);
+  updatePhysics();
+  render();
+
+}
+
+function updatePhysics() {
+
+  // Step the physics world
+  world.step(timeStep);
+
+  // Copy coordinates from Cannon.js to Three.js
+  mesh.position.copy(body.position);
+  mesh.quaternion.copy(body.quaternion);
+
+}
+
+function render() {
+
+  renderer.render(scene, camera);
+
+}
